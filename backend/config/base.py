686861c81e5c12dfd7c1c26488af9607984ac8b1
Ajay Chinnadurai <ajaychinnadurai@gmail.com>
2026-08-08 21:17:17 +0530

    fix: move CorsMiddleware to top of MIDDLEWARE list in base.py

--- backend/config/base.py
+++ backend/config/base.py
@@ -32,10 +32,10 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.middleware.gzip.GZipMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
